0e218bb05e62846ab1e5488609ed7d7b96d60a93
sunyon42 <114803704+sunyon42@users.noreply.github.com>
2022-12-15 22:38:47 +0900

    add
    add

--- 5. design/js/index.js
+++ 5. design/js/index.js
@@ -4,14 +4,18 @@ $(function(){
     var $offset = 500;
     var $deviceOST = $device.offset().top - $offset;
     var $featuresOST = $('.features').offest().top - $offset;
+    var $locationsOST = $('.location').offest().top - $offset;
 
     $(window).scroll(function(){
         if($(this).scrollTop() > $deviceOST){
             $device.find('img').addClass('animate');
         }
-        if($(this).scrollTop() > $deviceOST){
+        if($(this).scrollTop() > $featuresOST){
             $device.find('img').addClass('grow-img');
             $device.find('h4').addClass('animate');
+        }
+        if($(this).scrollTop() > $locationsOST){
+            $location.find('.stripe').addClass('animate_down');
         }
     });
 
