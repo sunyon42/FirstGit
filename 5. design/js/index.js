cb490a36d2e8e88ec3710538f6ba1ac43a0d1087
sunyon42 <114803704+sunyon42@users.noreply.github.com>
2022-12-15 19:14:20 +0900

    add
    add

--- 5. design/js/index.js
+++ 5. design/js/index.js
@@ -1,14 +1,19 @@
 $(function(){
 
     var $device = $('.device');
-    var $offset = 300;
+    var $offset = 500;
     var $deviceOST = $device.offset().top - $offset;
+    var $featuresOST = $('.features').offest().top - $offset;
 
     $(window).scroll(function(){
         if($(this).scrollTop() > $deviceOST){
             $device.find('img').addClass('animate');
         }
+        if($(this).scrollTop() > $deviceOST){
+            $device.find('img').addClass('grow-img');
+            $device.find('h4').addClass('animate');
+        }
     });
 
-    
+
 });
