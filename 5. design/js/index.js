da660b47ad123b5d1a0c813b23ec3120ea558dda
sunyon42 <114803704+sunyon42@users.noreply.github.com>
2022-12-17 01:31:45 +0900

    add
    add

--- 5. design/js/index.js
+++ 5. design/js/index.js
@@ -1,23 +1,26 @@
-$(function(){
-
-    var $device = $('.device');
-    var $offset = 500;
+$(function (){
+    var $device =$(`.device`);
+    var $features =$(`.features`);
+    var $location =$(`.location`);
+    var $offset=600;
     var $deviceOST = $device.offset().top - $offset;
-    var $featuresOST = $('.features').offest().top - $offset;
-    var $locationsOST = $('.location').offest().top - $offset;
+    var $featuresOST = $features.offset().top - $offset;
+    var $locationOST = $location.offset().top - $offset;
 
     $(window).scroll(function(){
-        if($(this).scrollTop() > $deviceOST){
-            $device.find('img').addClass('animate');
+        if($(this).scrollTop()>$deviceOST){
+            $device.find(`img`).addClass(`animate`);
         }
-        if($(this).scrollTop() > $featuresOST){
-            $device.find('img').addClass('grow-img');
-            $device.find('h4').addClass('animate');
+        if($(this).scrollTop()>$featuresOST){
+            $features.find(`img`).addClass(`grow-img`);
+            $features.find(`h4`).addClass(`animate`);
         }
-        if($(this).scrollTop() > $locationsOST){
-            $location.find('.stripe').addClass('animate_down');
+        if($(this).scrollTop()>$locationOST){
+            $location.find(`.stripe`).addClass(`animate_down`);
         }
+        
     });
+
 
 
 });
