bdd7e9f9ce82ca8e4778fc9c618d9a80c18c5d51
sunyon42 <114803704+sunyon42@users.noreply.github.com>
2022-12-26 12:32:20 +0900

    add
    add

--- 7.design/index.js
+++ 7.design/index.js
@@ -1,12 +1,12 @@
 $(function(){
 
     var $header = $('header');
-    var $item = $('.ability .item')
+    // var $item = $('.ability .item')
     var $strength =$(`.strength`);
     var $scroll =$(`.scroll-down`);
     var $offset = 600;
     var $strengthOST = $strength.offset().top - $offset;
-    var $itemThreshold = $item.offset().top -$offset;
+    // var $itemThreshold = $item.offset().top -$offset;
 
     $(window).scroll(function(){
         if($(this).scrollTop() > 0){
@@ -18,12 +18,33 @@ $(function(){
             $strength.find(`img`).addClass(`grow-img`);
             $strength.find(`h4`).addClass(`animate`);
         }
-        if($(this).scrollTop() > $itemThreshold){
-            $item.addClass('active');
-        }
+        // if($(this).scrollTop() > $itemThreshold){
+        //     $item.addClass('active');
+        // }
         if($(this).scrollTop() > 0){
             $scroll.addClass('scroll-op');
+        }else{
+            $scroll.removeClass('scroll-op');
         }
     });
 
+
+
+    var numAnimation = document.querySelectorAll('num_animation');
+    var targetNum = numAnimation.getAttribute('data-rate')
+
+
+    function changNum(idx){
+        var num = 0;
+        var targetNum = numAnimation[idx].getAttribute('date-rate');
+        var timer1 = seInterval(function(){
+            ++num;
+            numAnimation[idx].innerText = num;
+            if(num == targetNum){
+                clearInterval(timer1);
+            }
+        },10);
+    }
+    for(var i =0; i<numAnimation.length;i++);
+    changNum(i);
 });
